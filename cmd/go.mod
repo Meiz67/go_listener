f6237fc0b2e259b46ff924d76a9c0201d476ec4e
Meiz67 <meiz@inbox.ru>
2023-03-05 19:33:46 +0300

    consume messages

--- cmd/go.mod
+++ cmd/go.mod
@@ -2,4 +2,7 @@ module go_listener
 
 go 1.19
 
-require github.com/lib/pq v1.10.7
+require (
+	github.com/lib/pq v1.10.7
+	github.com/rabbitmq/amqp091-go v1.7.0
+)
--- cmd/main.go
+++ cmd/main.go
@@ -4,16 +4,63 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
+	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
 )
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	fmt.Println("Listener started")
 	db, _ := sql.Open("postgres", "port=5432 host=postgres user=postgres password=root dbname=postgres sslmode=disable")
 	err := db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		log.Println("Connected")
-	}
+	checkError(err)
+
+	conn, err := amqp.Dial("amqp://crm:Tm9oOGVlc29qYWhYMkNhago=@mq.l-wine.ru:5672")
+	checkError(err)
+
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	checkError(err)
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"test_go_consumer", // name
+		false,              // durable
+		false,              // delete when unused
+		false,              // exclusive
+		false,              // no-wait
+		nil,                // arguments
+	)
+
+	checkError(err)
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+
+	checkError(err)
+
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages...")
+	<-forever
+
 }
